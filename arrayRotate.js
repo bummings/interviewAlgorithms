43d627992ed3a76f1930e60ec43ae5de52c858df
bummings <eabummings@gmail.com>
2019-05-23 02:49:10 -0400

    adds instructions to array rotate

--- arrayRotate.js
+++ arrayRotate.js
@@ -1,3 +1,14 @@
+// Erica has two arrays of integers. THE FIRST consists
+// of distinct, positive integers and is her WORK ARRAY.
+// The second holds a list of integers DEFINING OPERATIONS
+// ON THE WORK ARRAY. In one left circular motion, the element
+// at index[0] is moved to the highest index of the array
+// and all of the elements shift left (down) by 1 index.
+
+// Given an array of distinct positive integers, perform each
+// rotation in the second array and determine the index of the
+// highest value element within the array.
+
 function getMaxElementIndexes(workArr, shift) {
   for (let x = 0; x < shift.length; x++) {
     // for each value in the shift array, do this:
@@ -12,4 +23,5 @@ function getMaxElementIndexes(workArr, shift) {
   }
 }
 
+// Here you go, Erica
 getMaxElementIndexes([1, 2, 3, 4, 5], [2, 3]);
